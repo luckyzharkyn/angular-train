9ed48f751938616f4c935fc214b5016a81c7d4b2
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2024-02-05 15:02:29 +0600

    закончил основы и синтаксис

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "Dynamic title";
-  number = 42;
+  title = "";
 
-  obj = {
-    a: 42,
-    b: 64
-  }
+  backgroundToggle = false;
 
-  inputValue = "";
-
-  constructor() {
-    
-  }
-
-  onInput(event: Event) {
-    console.log(event);
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-
-  changeInput(value: string) {
-    this.inputValue = value;
-  }
+  arr = [1, 2, 3, 4, 5];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './Post/post.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PostComponent } from './Post/post.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
